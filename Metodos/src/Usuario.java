8b410bb9769ceab6806993356456708418746ff1
cristianepagine <cristianepagine@gmail.com>
2023-05-05 10:30:48 -0300

    terminal+scanner

--- Metodos/src/Usuario.java
+++ Metodos/src/Usuario.java
@@ -2,6 +2,7 @@ public class Usuario {
     public static void main(String[] args) throws Exception {
         
         SmartTv smartTv = new SmartTv();
+        
 
         System.out.println("TV Ligada? " + smartTv.ligada);
         System.out.println("Qual canal? " + smartTv.canal);
